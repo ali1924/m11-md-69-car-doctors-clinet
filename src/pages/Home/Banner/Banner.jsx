cf91c60f57d3533b37abc3a30ceb3a427b7b536f
ali1924 <mohammadali229987@gmail.com>
2023-05-12 07:25:16 +0600

    69-4 Style Banner Component with gradient overlay

--- src/pages/Home/Banner/Banner.jsx
+++ src/pages/Home/Banner/Banner.jsx
@@ -19,12 +19,156 @@ const Banner = () => {
     return (
         <div>
             <div className="carousel w-full h-[600px]">
-                {
+                {/* {
                     sliders.map(slider => <BannerSlider
                         key={slider.id}
                         slider={slider}
                     ></BannerSlider>)
-                }
+                } */}
+                <div id='slide1' className="carousel-item relative w-full">
+                    <img src={img1} className="w-full rounded-lg" />
+                    <div className="absolute h-full flex items-center  left-0 right-0 bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0)]">
+                        <div className='text-white space-y-8 '>
+                            <h2 className='text-6xl font-bold'>
+                                Affordable <br />
+                                Price For Car <br />
+                                Servicing</h2>
+                            <p className='text-2xl'>
+                                There are many variations of passages of  available, but
+                                <br />
+                                the majority have suffered alteration in some form
+                            </p>
+                            <div>
+                                <button className="btn btn-warning mr-5">Discover More</button>
+                                <button className="btn btn-outline btn-secondary">Latest Project</button>
+                            </div>
+                            <div className=''>
+                                <a href="#slide6" className="btn btn-circle">❮</a>
+                                <a href="#slide2" className="btn btn-circle">❯</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div id='slide2' className="carousel-item relative w-full">
+                    <img src={img2} className="w-full rounded-lg" />
+                    <div className="absolute h-full flex items-center  left-0 right-0 bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0)]">
+                        <div className='text-white space-y-8 '>
+                            <h2 className='text-6xl font-bold'>
+                                Affordable <br />
+                                Price For Car <br />
+                                Servicing</h2>
+                            <p className='text-2xl'>
+                                There are many variations of passages of  available, but
+                                <br />
+                                the majority have suffered alteration in some form
+                            </p>
+                            <div>
+                                <button className="btn btn-warning mr-5">Discover More</button>
+                                <button className="btn btn-outline btn-secondary">Latest Project</button>
+                            </div>
+                            <div className=''>
+                                <a href="#slide1" className="btn btn-circle">❮</a>
+                                <a href="#slide3" className="btn btn-circle">❯</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div id='slide3' className="carousel-item relative w-full">
+                    <img src={img3} className="w-full rounded-lg" />
+                    <div className="absolute h-full flex items-center  left-0 right-0 bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0)]">
+                        <div className='text-white space-y-8 '>
+                            <h2 className='text-6xl font-bold'>
+                                Affordable <br />
+                                Price For Car <br />
+                                Servicing</h2>
+                            <p className='text-2xl'>
+                                There are many variations of passages of  available, but
+                                <br />
+                                the majority have suffered alteration in some form
+                            </p>
+                            <div>
+                                <button className="btn btn-warning mr-5">Discover More</button>
+                                <button className="btn btn-outline btn-secondary">Latest Project</button>
+                            </div>
+                            <div className=''>
+                                <a href="#slide2" className="btn btn-circle">❮</a>
+                                <a href="#slide4" className="btn btn-circle">❯</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div id='slide4' className="carousel-item relative w-full">
+                    <img src={img4} className="w-full rounded-lg" />
+                    <div className="absolute h-full flex items-center  left-0 right-0 bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0)]">
+                        <div className='text-white space-y-8 '>
+                            <h2 className='text-6xl font-bold'>
+                                Affordable <br />
+                                Price For Car <br />
+                                Servicing</h2>
+                            <p className='text-2xl'>
+                                There are many variations of passages of  available, but
+                                <br />
+                                the majority have suffered alteration in some form
+                            </p>
+                            <div>
+                                <button className="btn btn-warning mr-5">Discover More</button>
+                                <button className="btn btn-outline btn-secondary">Latest Project</button>
+                            </div>
+                            <div className=''>
+                                <a href="#slide3" className="btn btn-circle">❮</a>
+                                <a href="#slide5" className="btn btn-circle">❯</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div id='slide5' className="carousel-item relative w-full">
+                    <img src={img5} className="w-full rounded-lg" />
+                    <div className="absolute h-full flex items-center  left-0 right-0 bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0)]">
+                        <div className='text-white space-y-8 '>
+                            <h2 className='text-6xl font-bold'>
+                                Affordable <br />
+                                Price For Car <br />
+                                Servicing</h2>
+                            <p className='text-2xl'>
+                                There are many variations of passages of  available, but
+                                <br />
+                                the majority have suffered alteration in some form
+                            </p>
+                            <div>
+                                <button className="btn btn-warning mr-5">Discover More</button>
+                                <button className="btn btn-outline btn-secondary">Latest Project</button>
+                            </div>
+                            <div className=''>
+                                <a href="#slide4" className="btn btn-circle">❮</a>
+                                <a href="#slide6" className="btn btn-circle">❯</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div id='slide6' className="carousel-item relative w-full">
+                    <img src={img6} className="w-full rounded-lg" />
+                    <div className="absolute h-full flex items-center  left-0 right-0 bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0)]">
+                        <div className='text-white space-y-8 '>
+                            <h2 className='text-6xl font-bold'>
+                                Affordable <br />
+                                Price For Car <br />
+                                Servicing</h2>
+                            <p className='text-2xl'>
+                                There are many variations of passages of  available, but
+                                <br />
+                                the majority have suffered alteration in some form
+                            </p>
+                            <div>
+                                <button className="btn btn-warning mr-5">Discover More</button>
+                                <button className="btn btn-outline btn-secondary">Latest Project</button>
+                            </div>
+                            <div className=''>
+                                <a href="#slide5" className="btn btn-circle">❮</a>
+                                <a href="#slide1" className="btn btn-circle">❯</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     );
--- src/pages/Home/Banner/BannerSlider/BannerSlider.jsx
+++ src/pages/Home/Banner/BannerSlider/BannerSlider.jsx
@@ -8,7 +8,7 @@ const BannerSlider = (props) => {
         <>
             <div id={id} className="carousel-item relative w-full">
                 <img src={img} className="w-full rounded-lg" />
-                <div className="absolute h-full flex items-center left-5 right-5 bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0)]">
+                <div className="absolute h-full flex items-center  left-0 right-0 bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0)]">
                     <div className='text-white space-y-8 '>
                         <h2 className='text-6xl font-bold'>
                             Affordable <br />
@@ -23,9 +23,15 @@ const BannerSlider = (props) => {
                             <button className="btn btn-warning mr-5">Discover More</button>
                             <button className="btn btn-outline btn-secondary">Latest Project</button>
                         </div>
+                        <div className=''>
+                            <a href="#slide4" className="btn btn-circle">❮</a>
+                            <a href="#slide2" className="btn btn-circle">❯</a>
+                        </div>
                     </div>
-                    {/* <a href="#slide4" className="btn btn-circle">❮</a>
-                    <a href="#slide2" className="btn btn-circle">❯</a> */}
+                </div>
+                <div className=''>
+                    <a href="#slide4" className="btn btn-circle">❮</a>
+                    <a href="#slide2" className="btn btn-circle">❯</a>
                 </div>
             </div>
         </>
